670edfe073c8ab0c65b34dbc9b3f554691fecb32
Cássio Tieres Bomfim <68880880+Cassio-Tieres@users.noreply.github.com>
2020-12-12 20:36:54 -0300

    New Update
    2 atualização do projeto.

---
+++ New Unity Project/Assets/Script/GeradorDeArestas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorDeArestas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera cam = Camera.main;
+        EdgeCollider2D colisor = GetComponent<EdgeCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/Script/MovimentoPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoPlayer : MonoBehaviour
+{
+    public float velocidadeMovimento;
+    public float limiteDeX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionMouseX = Input.GetAxis("Mouse X"); // -1 =  esquerda; 0 = parado; 1 = direita
+        GetComponent<Transform>().position += Vector3.right * directionMouseX * velocidadeMovimento  * Time.deltaTime;
+        float xAtual = transform.position.x;
+        xAtual = Mathf.Clamp (xAtual, -limiteDeX, limiteDeX);
+        transform.position = new Vector3(xAtual, transform.position.y, transform.position.z);
+    }
+}
